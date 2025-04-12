1678369c13b8b96d43a30e9b086455aa4ba77be2
Mickykore <mikiyasayele6@gmail.com>
2025-04-12 20:47:14 +0300

    Add title for Event Management app

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -39,16 +39,18 @@ const Portfolio = () => {
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
                             <a href="https://www.embracevents.com/" target="_blank"><img className="img-fluid" src={embraceEvent} alt="..." /></a>
+                            <h5 className="text-center p-3" style={{ color: "black", fontWeight: "bold" }}>Event Management app</h5>
+
                         </div>
                     </div>
-                    <div className="col-md-6 mb-5 mt-5">
+                    {/* <div className="col-md-6 mb-5 mt-5">
                         <div className="portfolio-item">
                             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100 bg-dark">
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
                             <img className="img-fluid" src={portfolio1} alt="..." />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
